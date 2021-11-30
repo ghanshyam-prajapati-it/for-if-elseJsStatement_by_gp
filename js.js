f0c1008ca8dd7b58e89e625ea9ed0660d910f2e2
Ghanshyam Prajapati <89632088+ghanshyam-prajapati-it@users.noreply.github.com>
2021-11-30 16:14:37 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+for (var i=0; i<4; i++)
+{
+    if (i == 1)
+    {
+        document.write('"Hello, gp!"' + "<br>");
+    }
+    else
+    {
+        document.write("'Hello, World!'" + "<br>")
+    }
+}
